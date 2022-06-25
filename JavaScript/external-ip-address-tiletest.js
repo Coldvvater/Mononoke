1c603dcb85a8bbf9757b0d2b8ae46feb8dac8d08
Coldvvater <59505520+Coldvvater@users.noreply.github.com>
2022-06-25 13:22:46 +0800

    Update and rename external-ip-address-tiletest.js to nCov.js

--- JavaScript/external-ip-address-tiletest.js
+++
@@ -1,8 +0,0 @@
-$httpClient.get("https://api.my-ip.io/ip", function (error, response, data) {
-    $done({
-        title: "当前 IP 地址",
-        content: data,
-        backgroundColor: "#EECD39",
-        icon: "https:\/\/raw.githubusercontent.com\/shoujiqiyuan\/PokemonGOforQuanX\/master\/IconSet\/X001.png",
-    })
-})
