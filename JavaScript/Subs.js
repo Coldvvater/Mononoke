eed0d7e20925a95b85a4b4657d487f2a9db5d6c8
Coldvvater <59505520+Coldvvater@users.noreply.github.com>
2022-06-26 16:22:51 +0800

    Update and rename Subs.js to Subssample.js

--- JavaScript/Subs.js
+++
@@ -1,8 +0,0 @@
-$httpClient.get("https://zhs.futbol/link/dhz7qdfHAmGcnMNK?mu=4&tj=1", function (error, response, data) {
-    $done({
-        title: "当前用量",
-        content: data,
-        backgroundColor: "#318BD5",
-        icon: "network",
-    })
-})
