01d0f6ee46d73da7ad8eb3051ef8c4580b57035a
Coldvvater <59505520+Coldvvater@users.noreply.github.com>
2022-06-26 12:40:52 +0800

    Update and rename nCov.js to Timecard.js

---
+++ JavaScript/Timecard.js
@@ -0,0 +1,88 @@
+$httpClient.get("https://www.mxnzp.com/api/holiday/single/", function (error, response, data) {
+    $done({
+        title: "节假日倒计时",
+        content: tlist[nowlist][0]+"  :  "+today(tnumcount(nowlist))+"天\n"+tlist[Number(nowlist) + Number(1)][0] +"  :  "+ tnumcount(Number(nowlist) + Number(1))+ "天\n"+tlist[Number(nowlist) + Number(2)][0]+"  :  "+tnumcount(Number(nowlist) + Number(2))+"天"
+}),
+        backgroundColor: "#5AC8FA",
+        icon: "list.dash.header.rectangle",
+    })
+})
+var tlist = {
+  1: ["生日", "2022-04-20"],
+  2: ["劳动", "2022-05-01"],
+  3: ["奶奶生日", "2022-05-04"],
+  4: ["楠楠生日", "2022-05-14"],
+  5: ["520", "2022-05-20"],
+  6: ["521", "2022-05-21"],
+  7: ["端午", "2022-06-03"],
+  8: ["WWDC22", "2022-06-07"],
+  9: ["老婆生日", "2022-07-03"],
+  10: ["七夕", "2022-08-04"],
+  11: ["多多生日", "2022-08-20"],
+  12: ["中秋", "2022-09-10"],
+  13: ["国庆", "2022-10-01"],
+  14: ["元旦", "2023-01-01"],
+  15: ["春节", "2023-01-22"],
+  16: ["元宵", "2023-02-05"],
+  18: ["结婚纪念日3", "2023-03-27"],
+  19: ["清明", "2023-04-05"]
+};
+let tnow = new Date();
+let tnowf =
+  tnow.getFullYear() + "-" + (tnow.getMonth() + 1) + "-" + tnow.getDate();
+
+/* 计算2个日期相差的天数，不包含今天，如：2016-12-13到2016-12-15，相差2天
+ * @param startDateString
+ * @param endDateString
+ * @returns
+ */
+function dateDiff(startDateString, endDateString) {
+  var separator = "-"; //日期分隔符
+  var startDates = startDateString.split(separator);
+  var endDates = endDateString.split(separator);
+  var startDate = new Date(startDates[0], startDates[1] - 1, startDates[2]);
+  var endDate = new Date(endDates[0], endDates[1] - 1, endDates[2]);
+  return parseInt(
+    (endDate - startDate) / 1000 / 60 / 60 / 24
+  ).toString();
+}
+
+//计算输入序号对应的时间与现在的天数间隔
+function tnumcount(num) {
+  let dnum = num;
+  return dateDiff(tnowf, tlist[dnum][1]);
+}
+
+//获取最接近的日期
+function now() {
+  for (var i = 1; i <= Object.getOwnPropertyNames(tlist).length; i++) {
+    if (Number(dateDiff(tnowf, tlist[i.toString()][1])) >= 0) {
+      //console.log("最近的日期是:" + tlist[i.toString()][0]);
+      //console.log("列表长度:" + Object.getOwnPropertyNames(tlist).length);
+      //console.log("时间差距:" + Number(dateDiff(tnowf, tlist[i.toString()][1])));
+      return i;
+    }
+  }
+}
+
+//如果是0天，发送emoji;
+let nowlist = now();
+function today(day) {
+  let daythis = day;
+  if (daythis == "0") {
+    datenotice();
+    return "🎉";
+  } else {
+    return daythis;
+  }
+}
+
+//提醒日当天发送通知
+function datenotice() {
+  if ($persistentStore.read("timecardpushed") != tlist[nowlist][1] && tnow.getHours() >= 6) {
+    $persistentStore.write(tlist[nowlist][1], "timecardpushed");
+    $notification.post("假日祝福","", "今天是" + tlist[nowlist][1] + "日 " + tlist[nowlist][0] + "   🎉")
+  } else if ($persistentStore.read("timecardpushed") == tlist[nowlist][1]) {
+    //console.log("当日已通知");
+  }
+}
