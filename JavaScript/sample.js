623ca0c5414ebc2ab681539d44267ca38f980254
Coldvvater <59505520+Coldvvater@users.noreply.github.com>
2022-06-28 13:34:49 +0800

    Update and rename suibian.js to sample.js

---
+++ JavaScript/sample.js
@@ -0,0 +1,8 @@
+$httpClient.get("", function (error, response, data) {
+    $done({
+        title: "",
+        content: data,
+        backgroundColor: "#318BD5",
+        icon: ",
+    })
+})
--- JavaScript/suibian.js
+++
@@ -1,8 +0,0 @@
-$httpClient.get("https://c-ssl.duitang.com/uploads/item/202004/26/20200426131631_taysd.jpg", function (error, response, data) {
-    $done({
-        title: "随便了",
-        content: data,
-        backgroundColor: "#318BD5",
-        icon: "cloud.sun.rain.fill.system",
-    })
-})
