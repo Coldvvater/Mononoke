1ffe97f70a14a7ae8f705f6457a0a6d104e0c6c0
Coldvvater <59505520+Coldvvater@users.noreply.github.com>
2022-06-28 13:30:32 +0800

    Update and rename weather.js to suibian.js

--- JavaScript/suibian.js
+++ JavaScript/suibian.js
@@ -1,6 +1,6 @@
 $httpClient.get("https://c-ssl.duitang.com/uploads/item/202004/26/20200426131631_taysd.jpg", function (error, response, data) {
     $done({
-        title: "当前天气",
+        title: "随便了",
         content: data,
         backgroundColor: "#318BD5",
         icon: "cloud.sun.rain.fill.system",
