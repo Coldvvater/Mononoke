38a2db6614820eba0a16db144a1c36517de605ea
Coldvvater <59505520+Coldvvater@users.noreply.github.com>
2022-06-30 18:51:08 +0800

    Update covid19.js

--- JavaScript/covid19.js
+++ JavaScript/covid19.js
@@ -46,7 +46,7 @@ $httpClient.get(url, function(error, response, data){
      $done({
        title: "疫情查询:"+ToDBC("  ")+"新增|现存"+ToDBC("  ")+nowtime(),
        content: ala.replace(/\n$/, ""),
-       icon: "heart.circle",
+       icon: "aqi.medium",
        backgroundColor: '#18CDCD',
      });
     }
