499c352a414d9f26f342fc60eb0212ac55c0c626
Coldvvater <59505520+Coldvvater@users.noreply.github.com>
2022-06-30 12:58:14 +0800

    Update weekly_notes.js

--- JavaScript/weekly_notes.js
+++ JavaScript/weekly_notes.js
@@ -1,3 +1,11 @@
+/*
+ * 原脚本作者: smartmimi
+ * 原脚本地址：https://github.com/smartmimi/conf/blob/master/surge/weekly_notes.js
+ * 由@Coldvvater修改
+ * 更新日期：2022.06.30
+ * 版本：1.0
+ */
+
 const weekly_notes_boxjs_title = $persistentStore.read("weekly_notes_title");
 var tnow = new Date();
 var tnoww = tnow.getDay();
