92d3f9f0b47e5db8463616f514398803cb1871ae
Coldvvater <59505520+Coldvvater@users.noreply.github.com>
2022-07-08 16:00:58 +0800

    Update stream.js

--- JavaScript/stream.js
+++ JavaScript/stream.js
@@ -32,7 +32,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
       title: '流媒体解锁检测',
       content: '',
       icon: 'play.tv.fill',
-      backgroundColor: '#FF2D55',
+      backgroundColor: '#E40814',
     }
     let [{ region, status }] = await Promise.all([testDisneyPlus()])
     await Promise.all([check_youtube_premium(), check_netflix()])
